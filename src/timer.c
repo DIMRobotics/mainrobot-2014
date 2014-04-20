b8680fcc91d680bece8e19e71fe5b00cb1c708f9
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-04-20 19:36:28 +0400

    Add countdown timer
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- src/timer.c
+++ src/timer.c
@@ -13,9 +13,24 @@ ANTARES_INIT_LOW(maintimer_init)
         TIMSK0 |= (1<<OCIE0A);
 }
 
+static volatile int32_t timer = -1;
+
+void timer_run(void)
+{
+        timer = 91000;
+}
+
 ISR(TIMER0_COMPA_vect)
 {
         sei();
         tmgr_interrupt();
         chassis_interrupt();
+        if (timer >= 0) {
+                timer--;
+                
+                if (timer == 0) {
+                        GPIO_WRITE_HIGH(CONFIG_ROBOT_VACUUM);
+                        while (1);; /* robot shutdown */
+                }
+        }
 }
