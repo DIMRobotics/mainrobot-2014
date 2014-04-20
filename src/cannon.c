1458f488b4330c5b9fc567357ffa662d480b0f94
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-04-20 16:12:39 +0400

    Cannon modification
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- src/cannon.c
+++ src/cannon.c
@@ -2,16 +2,16 @@
 
 #include <robot/servo.h>
 
-ANTARES_INIT_HIGH(cannon_init)
+ANTARES_INIT_LOW(cannon_init)
 {
-        servo_write(3, 70);
-        servo_write(4, 110);
+        servo_write(3, 140);
+        servo_write(4, 0);
 }
 
 void cannon_release(uint8_t cannon)
 {
         if (cannon == CANNON_LEFT)
-                servo_write(3, 0);
+                servo_write(3, 100);
         else if (cannon == CANNON_RIGHT)
-                servo_write(4, 180);
+                servo_write(4, 50);
 }
