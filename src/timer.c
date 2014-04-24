d0b65cca1ac6f8c6969ae2a3be05929833108e03
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-04-24 11:02:39 +0400

    Add e-stop after 90 seconds
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- src/timer.c
+++ src/timer.c
@@ -33,6 +33,9 @@ ISR(TIMER0_COMPA_vect)
                 
                 if (timer == 0) {
                         GPIO_WRITE_HIGH(CONFIG_ROBOT_VACUUM);
+                        stepper_set_speed(1, 0);
+                        stepper_set_speed(2, 0);
+                        stepper_set_speed(3, 0);
                         while (1);; /* robot shutdown */
                 }
         }
